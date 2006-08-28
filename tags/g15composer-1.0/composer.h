f63961739d8e97cc669037254f4c2da86c1b67cc
aneurysm9 <aneurysm9@f3f12dcf-2114-0410-a68b-d6aaca122e96>
2006-08-28 03:05:38 +0000

    Tag g15composer-1.0 for release

---
+++ tags/g15composer-1.0/composer.h
@@ -0,0 +1,17 @@
+#ifndef COMPOSER_H_
+#define COMPOSER_H_
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void handlePixelCommand(std::string const &input_line);
+void handleModeCommand(std::string const &input_line);
+void handleTextCommand(std::string const &input_line);
+void updateScreen(bool);
+int get_params(int*, std::string const &, int, int);
+void fifoProcessingWorkflow(bool is_script, string const &filename);
+
+
+#endif /*COMPOSER_H_*/
